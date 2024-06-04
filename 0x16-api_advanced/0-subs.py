cea7392ad6e83366d789c722c4089d17dd67b25f
A1iHassan <ali012wkout@gmail.com>
2024-06-04 21:44:18 +0300

    a commit by Ali Hassan

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,8 @@
 
 def number_of_subscribers(subreddit):
     """returns number of total subscribers"""
+    import requests
+
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
